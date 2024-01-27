98030f35a7c3072e8a63c98fd0e23e8418369938
lxxonx <leeonechang92@gmail.com>
2024-01-27 13:23:24 +0900

    setup

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Response, Router } from 'express';
-
-const router = Router();
-
-router.get('/', (_, res: Response) => {
-  res.send('api!');
-});
-
-export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
-import api from './api';
+import api from './api/controller';
 
 const app = express();
 const port = 3000;
